4cc5a854c7e76a870502c1dfc43d4d2ae7406cde
yzoni <yzonix@gmail.com>
2017-05-18 16:59:21 +0200

    refactor parameter search

--- search_parameters/main.py
+++ search_parameters/main.py
@@ -1,15 +1,16 @@
-from parameter_search import degree_distribution_parameter_search
-from parameter_search import discrete_generalized_log_normal_probability
-from parameter_search import generate_degree_distribution
+from search_parameters.parameter_search import ParameterSearch
 
 if __name__ == '__main__':
-    nnodes = 20
-    maxdeg_bound = 10
-    avgdeg_target = 3
-    maxccd_target = 0.95
+    number_of_nodes = 20
+    max_deg_bound = 10
+    avg_deg_target = 3
+    max_ccd_target = 0.95
     gcc_target = 0.15
-    tau = 1e-3 / nnodes
+    tau = 1e-3 / number_of_nodes
 
-    alpha, beta = degree_distribution_parameter_search(maxdeg_bound, tau, avgdeg_target)
-    pdf = discrete_generalized_log_normal_probability(nnodes, alpha, beta)
-    nd = generate_degree_distribution(nnodes, pdf)
+    p = ParameterSearch(number_of_nodes, max_deg_bound, avg_deg_target, max_ccd_target, gcc_target)
+
+    alpha, beta = p.degree_distribution_parameter_search(tau)
+    pdf = p.discrete_generalized_log_normal_probability(alpha, beta)
+    nd = p.generate_degree_distribution(pdf)
+    p.write_to_file('parameters.txt')
--- search_parameters/parameter_search.py
+++ search_parameters/parameter_search.py
@@ -1,71 +1,87 @@
-import scipy.optimize
 import numpy as np
-
-def evaluate_degree_distribution(alpha, beta, maxdeg, bnd, avgdeg):
-    p = discrete_generalized_log_normal_probability(maxdeg, alpha, beta)
-    if p[-1] > bnd:
-        y1 = np.power((np.exp(1 + p[-1] - bnd)), 2) - 1
-    else:
-        y1 = 0
-
-    a = np.dot(np.linspace(1, maxdeg, maxdeg), p)
-    y2 = np.power(np.subtract(a, avgdeg), 2)
-
-    y = y1 + y2
-
-    return y
+import scipy.optimize
 
 
-def discrete_generalized_log_normal_probability(n, alpha, beta):
-    """
-    Discrete generalized log-normal probability density function
+class ParameterSearch:
+    def __init__(self, number_of_nodes, max_deg_bound, avg_deg_target, max_ccd_target, gcc_target):
+        self.n = number_of_nodes
+        self.max_deg_bound = max_deg_bound
+        self.avg_deg_target = avg_deg_target
+        self.max_ccd_target = max_ccd_target
+        self.gcc_target = gcc_target
 
-    :param n:
-    :param alpha:
-    :param beta:
-    :return:
-    """
-    N = np.log(np.transpose(np.linspace(1, n, n)))
-    p = np.exp(-np.power(np.divide(N, alpha), beta))
-    return np.divide(p, np.sum(p))
+        self.ccd = []
+        self.nd = []
 
+    def evaluate_degree_distribution(self, alpha, beta, bnd, avgdeg):
+        p = self.discrete_generalized_log_normal_probability(alpha, beta)
 
-def degree_distribution_parameter_search(maxdeg, tau, avgdeg):
-    handle = lambda x: evaluate_degree_distribution(x[0], x[1], maxdeg, tau, avgdeg)
-    xopt = scipy.optimize.fmin(func=handle, x0=[2, 2])
-    return xopt
+        if p[-1] > bnd:
+            y1 = np.power((np.exp(1 + p[-1] - bnd)), 2) - 1
+        else:
+            y1 = 0
 
+        a = np.dot(np.linspace(1, self.max_deg_bound, self.max_deg_bound), p)
+        y2 = np.power(np.subtract(a, avgdeg), 2)
 
-def generate_degree_distribution(n, pdf, cutoff=0):
-    """
-    Create a random degree distribution from a given PDF
+        y = y1 + y2
 
-    :param n: Number of nodes
-    :param pdf: Probability density function
-    :param cutoff:
-    :return:
-    """
-    n1 = np.sum(cutoff) # Todo
-    dd1 = []
+        return y
 
+    def discrete_generalized_log_normal_probability(self, alpha, beta):
+        """
+        Discrete generalized log-normal probability density function
+    
+        :param alpha:
+        :param beta:
+        :return:
+        """
+        N = np.log(np.transpose(np.linspace(1, self.max_deg_bound, self.max_deg_bound)))
+        p = np.exp(-np.power(np.divide(N, alpha), beta))
+        return np.divide(p, np.sum(p))
 
-    n2 = n - n1
-    tail_pdf = pdf[cutoff:] / np.sum(pdf[cutoff:])
-    tail_cdf = np.cumsum(tail_pdf)
-    idx2 = np.where(tail_cdf < 1)[0][-1]
-    tail_cdf = np.insert(tail_cdf[:idx2], 0, 0)
-    tail_cdf = np.append(tail_cdf, 1)
-    coins = np.random.rand(n2, 1)
+    def degree_distribution_parameter_search(self, tau, avgdeg):
+        handle = lambda x: self.evaluate_degree_distribution(x[0], x[1], tau, avgdeg)
+        xopt = scipy.optimize.fmin(func=handle, x0=[2, 2])
+        return xopt
 
-    cnts = np.histogram(coins, tail_cdf)
+    def generate_degree_distribution(self, pdf, cutoff=0):
+        """
+        Create a random degree distribution from a given PDF
+    
+        :param pdf: Probability density function
+        :param cutoff:
+        :return:
+        """
+        n1 = np.sum(cutoff)  # Todo
+        dd1 = []
 
-    idx3 = np.where(cnts[0] > 0)[0][-1]
-    dd2 = cnts[0][:idx3]
+        n2 = self.n - n1
+        tail_pdf = pdf[cutoff:] / np.sum(pdf[cutoff:])
+        tail_cdf = np.cumsum(tail_pdf)
+        idx2 = np.where(tail_cdf < 1)[0][-1]
+        tail_cdf = np.insert(tail_cdf[:idx2], 0, 0)
+        tail_cdf = np.append(tail_cdf, 1)
+        coins = np.random.rand(n2, 1)
 
-    # return concatination of dd1 and dd2
-    return np.append(dd1, dd2)
+        cnts = np.histogram(coins, tail_cdf)
 
-def optimal_xi(nd):
-    max_degree = np.where(nd > 0.1)[0][-1]
+        idx3 = np.where(cnts[0] > 0)[0][-1]
+        dd2 = cnts[0][:idx3]
 
-    pass
+        return np.append(dd1, dd2)
+
+    def optimal_xi(self, nd):
+        max_degree = np.where(nd > 0.1)[0][-1]
+
+        pass
+
+    def write_to_file(self, file_name):
+        with open(file_name, 'w') as f:
+            f.write(self.n + '\n')
+            for e in self.nd:
+                f.write(e + ' ')
+            f.write('\n')
+            for e in self.ccd:
+                f.write(e + ' ')
+            f.write('\n')
